54fb2a73507e839bde0559a7cc5f7acfc71337e2
ynfeng <fynwin@gmail.com>
2018-06-19 14:18:14 +0800

    mybatis生成器集成

---
+++ src/main/java/com/teamsking/domain/repository/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.teamsking.domain.repository;
+
+import com.teamsking.domain.entity.SysUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SysUserMapper extends Mapper<SysUser> {
+}
