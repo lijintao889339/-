de810cd7f41a14ccefe94b7c761a582f7a5eb240
linhao <qbc4331@dingtalk.com>
2018-07-20 11:41:47 +0800

    添加课程老师关系实体类及映射文件、业务层

---
+++ src/main/java/com/teamsking/domain/service/course/CourseTeacherConnectionService.java
@@ -0,0 +1,31 @@
+package com.teamsking.domain.service.course;
+
+import com.teamsking.domain.entity.course.CourseTeacherConnection;
+import com.teamsking.domain.repository.CourseTeacherConnectionMapper;
+import com.teamsking.domain.service.BaseService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+/**
+*@author linhao
+*/
+
+@Service
+@Slf4j
+public class CourseTeacherConnectionService extends BaseService {
+
+    @Autowired
+    CourseTeacherConnectionMapper courseTeacherConnectionMapper;
+
+    public List<CourseTeacherConnection> getTeacherByCourseIdList(List<Integer> courseIds) {
+
+        Example courseTeacherConnectionExample = new Example(CourseTeacherConnection.class);
+        Example.Criteria cri = courseTeacherConnectionExample.createCriteria();
+        cri.andIn("courseId",courseIds);
+        return courseTeacherConnectionMapper.selectByExample(courseTeacherConnectionExample);
+    }
+}
