ca3cff31b4bac9c4c4bb85a0d19f01b22511218e
lijintao <lijintao0352@dingtalk.com>
2018-07-27 14:35:58 +0800

    生成了班课-教师关系实体

---
+++ src/main/java/com/teamsking/domain/repository/OpenTeacherConnectionMapper.java
@@ -0,0 +1,7 @@
+package com.teamsking.domain.repository;
+
+import com.teamsking.domain.entity.open.OpenTeacherConnection;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OpenTeacherConnectionMapper extends Mapper<OpenTeacherConnection> {
+}
