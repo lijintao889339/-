d1f673afcd0bdf9827b52d9955c74a4e78a3f4fd
linhao <qbc4331@dingtalk.com>
2018-08-14 13:58:39 +0800

    增加辅导老师和分组表及学生和辅导老师表添加学校Id字段

---
+++ src/main/java/com/teamsking/domain/repository/UserTeacherGroupMapper.java
@@ -0,0 +1,7 @@
+package com.teamsking.domain.repository;
+
+import com.teamsking.domain.entity.sys.UserTeacherGroup;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserTeacherGroupMapper extends Mapper<UserTeacherGroup> {
+}
