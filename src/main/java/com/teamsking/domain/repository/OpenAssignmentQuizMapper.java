a6e15a0b60d8f1c5bb1d7e2da91140ae99b4348e
linhao <qbc4331@dingtalk.com>
2018-08-28 14:26:22 +0800

    添加试题和作业及学生和作业下的试题关系表

---
+++ src/main/java/com/teamsking/domain/repository/OpenAssignmentQuizMapper.java
@@ -0,0 +1,7 @@
+package com.teamsking.domain.repository;
+
+import com.teamsking.domain.entity.open.OpenAssignmentQuiz;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OpenAssignmentQuizMapper extends Mapper<OpenAssignmentQuiz> {
+}
