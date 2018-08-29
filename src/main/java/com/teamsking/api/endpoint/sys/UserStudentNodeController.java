ce08fec6d47d0bd7380f3db9f7f0d7275951332f
linhao <qbc4331@dingtalk.com>
2018-08-29 15:07:55 +0800

    根据班组搜索学生观看视频时长列表接口

--- src/main/java/com/teamsking/api/endpoint/sys/UserStudentNodeController.java
+++ src/main/java/com/teamsking/api/endpoint/sys/UserStudentNodeController.java
@@ -27,7 +27,7 @@ public class UserStudentNodeController extends BaseController {
     @Autowired
     UserStudentNodeService userStudentNodeService;
 
-    @ApiOperation(value = "班课管理列表", notes = "可分页", produces = "application/json")
+    @ApiOperation(value = "学生观看视频时长列表", notes = "可分页", produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "pageNo", paramType = "query",value = "页码", required = true, example = "1"),
             @ApiImplicitParam(name = "pageSize", paramType = "query", value = "页大小", required = true, example = "10"),
@@ -39,6 +39,30 @@ public class UserStudentNodeController extends BaseController {
                                       @PathVariable int nodeId, @PathVariable int openId) {
 
         Page page = userStudentNodeService.list(fixPage(pageNo),fixPage(pageSize),nodeId,openId);
+        if (null == page){
+            return Result.success().addData("pager", null);
+        }else {
+            return Result.success().addData("pager", warpPage(page));
+        }
+
+
+    }
+
+
+
+    @ApiOperation(value = "根据班组搜索学生观看视频时长", notes = "可分页", produces = "application/json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNo", paramType = "query",value = "页码", required = true, example = "1"),
+            @ApiImplicitParam(name = "pageSize", paramType = "query", value = "页大小", required = true, example = "10"),
+            @ApiImplicitParam(name = "nodeId", value = "视频的主键", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "openId", value = "班课的主键", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "groupId", value = "班组的主键", required = true, dataType = "int")
+    })
+    @GetMapping("/searching_userStudentNodes/{openId}/{groupId}/{nodeId}")
+    public Result userStudentNodeListByGroupId(@RequestParam int pageNo, @RequestParam int pageSize, @PathVariable int groupId,
+                                      @PathVariable int nodeId, @PathVariable int openId) {
+
+        Page page = userStudentNodeService.listByGroupId(fixPage(pageNo),fixPage(pageSize),nodeId,openId,groupId);
         if (null == page){
             return Result.success().addData("pager", null);
         }else {
--- src/main/java/com/teamsking/domain/service/sys/UserStudentNodeService.java
+++ src/main/java/com/teamsking/domain/service/sys/UserStudentNodeService.java
@@ -6,16 +6,20 @@ import com.google.common.collect.Lists;
 import com.teamsking.api.dto.sys.UserStudentNodeDto;
 import com.teamsking.api.dto.sys.UserStudentNodeDtoMapper;
 import com.teamsking.domain.entity.node.Node;
+import com.teamsking.domain.entity.open.OpenGroup;
 import com.teamsking.domain.entity.open.OpenUser;
 import com.teamsking.domain.entity.sys.UserStudent;
 import com.teamsking.domain.entity.sys.UserStudentNode;
 import com.teamsking.domain.repository.NodeMapper;
+import com.teamsking.domain.repository.OpenGroupMapper;
+import com.teamsking.domain.repository.OpenUserMapper;
 import com.teamsking.domain.repository.UserStudentNodeMapper;
 import com.teamsking.domain.service.BaseService;
 import com.teamsking.domain.service.open.OpenUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import java.text.NumberFormat;
 import java.util.List;
@@ -28,6 +32,10 @@ public class UserStudentNodeService extends BaseService {
     UserStudentNodeMapper userStudentNodeMapper;
     @Autowired
     NodeMapper nodeMapper;
+    @Autowired
+    OpenUserMapper openUserMapper;
+    @Autowired
+    OpenGroupMapper openGroupMapper;
 
     @Autowired
     UserStudentService userStudentService;
@@ -110,6 +118,102 @@ public class UserStudentNodeService extends BaseService {
             Page page = null;
             return page;
         }
+    }
 
+
+    /**
+     * 获取学生观看视频时长列表
+     * @param pageNo
+     * @param pageSize
+     * @param nodeId
+     * @param openId
+     * @param groupId
+     * @return
+     */
+    public Page listByGroupId(int pageNo, int pageSize, int nodeId, int openId, Integer groupId) {
+
+        //该班组信息
+        OpenGroup openGroup = openGroupMapper.selectByPrimaryKey(groupId);
+
+        //获取该视频时长
+        Node node = nodeMapper.selectByPrimaryKey(nodeId);
+        Integer seconds = node.getSeconds();
+
+        //获取班组下面的学生
+        OpenUser openUser = new OpenUser();
+        openUser.setOpenId(openId);
+        openUser.setGroupId(groupId);
+        openUser.setDeleteStatus(2);
+        List<OpenUser> openUserList = openUserMapper.select(openUser);
+
+        if (0 != openUserList.size()){
+
+            //获取学生Ids
+            List<Integer> userStudentIds = Lists.newArrayList();
+            for (OpenUser newOpenUser : openUserList){
+                userStudentIds.add(newOpenUser.getUserStudentId());
+            }
+
+            //分页
+            PageHelper.startPage(pageNo,pageSize);
+
+            //获取该视频的学生观看时长信息
+            Example studentNodeExample = new Example(UserStudentNode.class);
+            Example.Criteria cir = studentNodeExample.createCriteria();
+            cir.andEqualTo("nodeId",nodeId);
+            cir.andEqualTo("deleteStatus",2);
+            cir.andIn("userStudentId",userStudentIds);
+            List<UserStudentNode> userStudentNodeList = userStudentNodeMapper.selectByExample(studentNodeExample);
+
+            if (0 != userStudentNodeList.size()){
+
+
+
+                //获取学生信息
+                List<UserStudent> userStudentList = userStudentService.getUserStudentListByIds(userStudentIds);
+
+                //获取学生选课信息(包含班组信息)
+                //List<OpenUser> openUserList = openUserService.getOpenUserLIstByStudentIds(userStudentIds,openId);
+
+                //数据转换
+                List<UserStudentNodeDto> userStudentNodeDtoList = UserStudentNodeDtoMapper.INSTANCE.entityListToDtoList(userStudentNodeList);
+
+                //遍历集合，组装数据
+                for (UserStudentNodeDto studentNodeDto :  userStudentNodeDtoList){
+
+                    //获取用户名
+                    for (UserStudent userStudent : userStudentList){
+                        if (studentNodeDto.getUserStudentId().intValue() == userStudent.getId().intValue()){
+                            studentNodeDto.setUserName(userStudent.getUserName());
+                        }
+                    }
+
+                    //获取组名
+                    studentNodeDto.setGroupName(openGroup.getGroupName());
+
+                    //获取观看时长
+                    Integer watchSeconds = studentNodeDto.getWatchSeconds();
+                    //创建一个数值格式化对象
+                    NumberFormat numberFormat = NumberFormat.getInstance();
+                    //设置精确到小数点后2位
+                    numberFormat.setMaximumFractionDigits(0);
+                    //转化为百分比
+                    String watchRate = numberFormat.format( (float)watchSeconds / (float)seconds * 100) + "%";
+
+                    //获取观看进度
+                    studentNodeDto.setWatchRate(watchRate);
+                }
+
+                return convertPage((Page)userStudentNodeList, userStudentNodeDtoList);
+
+            }else {
+                Page page = null;
+                return page;
+            }
+
+        }else {
+            Page page = null;
+            return page;
+        }
     }
 }
