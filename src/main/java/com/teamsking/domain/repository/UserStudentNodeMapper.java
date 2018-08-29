3fd391355e857b722bc4efde9aa4961fea7d98b4
linhao <qbc4331@dingtalk.com>
2018-08-29 10:15:14 +0800

    添加学生观看视频时长表

---
+++ src/main/java/com/teamsking/domain/repository/UserStudentNodeMapper.java
@@ -0,0 +1,7 @@
+package com.teamsking.domain.repository;
+
+import com.teamsking.domain.entity.sys.UserStudentNode;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserStudentNodeMapper extends Mapper<UserStudentNode> {
+}
